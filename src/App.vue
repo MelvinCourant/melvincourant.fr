f96d540e887a53e01a78731f97a66da59e90a12f
Melvin <melvin.courant@lasuiteandco.com>
2023-08-17 10:37:47 +0200

    :tada: Init project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+  Hello World
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+html {
+  min-height: 100%;
+
+  body {
+    min-height: 100%;
+
+    video,
+    img,
+    svg,
+    object {
+      max-width: 100%;
+      height: auto;
+      vertical-align: middle;
+    }
+
+    li {
+      list-style: none;
+    }
+
+    a {
+      text-decoration: none;
+    }
+  }
+}
+</style>
