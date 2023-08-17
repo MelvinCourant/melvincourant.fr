7e11f8efa422f2e3bf42987556f005eabd8399b7
Melvin <melvin.courant@lasuiteandco.com>
2023-08-17 11:24:30 +0200

    :lipstick: Add global styles, fonts and setup alias

---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="hero">
+    <h1>Melvin Courant</h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.hero {
+  height: 100vh;
+}
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h2>Compétences</h2>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
