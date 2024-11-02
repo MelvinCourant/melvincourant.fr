a5f6e87209418c82d26f33d94fe0b4a33fa51221
MelvinCourant <melvin.courant1@gmail.com>
2024-11-02 20:13:22 +0100

    :lipstick: add hero left style

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,12 +1,18 @@
 <script setup>
+import "../../assets/css/sections/_hero.scss";
 import CTA from "../utils/CTA.vue";
 </script>
 
 <template>
   <section class="hero">
     <div class="hero__left">
-      <h1 class="hero__name">Melvin<br> Courant</h1>
-      <p class="hero__description">Développeur front-end</p>
+      <div class="hero__texts">
+        <h1 class="hero__name">
+          <span class="hero__firstname">Melvin</span><br>
+          Courant
+        </h1>
+        <p class="hero__description">Développeur front-end</p>
+      </div>
       <CTA content="Découvre moi !" url="#about" />
     </div>
   </section>
