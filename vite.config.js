425c39f7a2ee3aa475deff365aa00b10bed9f74d
MelvinCourant <melvin.courant1@gmail.com>
2024-11-02 17:36:16 +0100

    :tada: init new style

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@css': fileURLToPath(new URL('./src/assets/css', import.meta.url)),
-      '@imgs': fileURLToPath(new URL('./src/assets/imgs', import.meta.url)),
-    },
-  },
   server: {
     open: true,
   }
