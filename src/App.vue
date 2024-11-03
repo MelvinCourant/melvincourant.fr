ec715578c28fc370c961e9531b1e8df1cc6f2117
MelvinCourant <melvin.courant1@gmail.com>
2024-11-03 21:29:18 +0100

    :lipstick: style to navbar

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import "./assets/css/main.scss";
 import "./components/sections/hero/Hero.vue";
 import Hero from "./components/sections/hero/Hero.vue";
+import Navbar from "./components/layouts/Navbar.vue";
 </script>
 
 <template>
+  <Navbar />
   <main>
     <Hero />
   </main>
