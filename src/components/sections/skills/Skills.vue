90fd6c42d5d1c18466e641c9fad990d3b1b4519f
MelvinCourant <melvin.courant1@gmail.com>
2024-11-23 12:18:55 +0100

    :lipstick: create json file to skills, style to skills list and skill card

--- src/components/sections/skills/Skills.vue
+++ src/components/sections/skills/Skills.vue
@@ -1,7 +1,11 @@
 <script setup>
-
+import "../../../assets/css/sections/skills/_skills.scss";
+import skillsJson from "../../../data/skills.json";
+import SkillsList from "./SkillsList.vue";
 </script>
 
 <template>
-  <h1>Skills</h1>
+  <section class="skills" id="skills">
+    <SkillsList :skills="skillsJson" />
+  </section>
 </template>
---
+++ src/components/sections/skills/SkillsList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import "../../../assets/css/sections/skills/_skills-list.scss";
+import Skill from "./Skill.vue";
+
+defineProps({
+  skills: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ul class="skills-list">
+    <Skill
+      v-for="(skill, index) in skills"
+      :key="index"
+      :skill="skill"
+    />
+  </ul>
+</template>
